97e5ba9356d3f090aa8b5d49bb0923873cf413d2
sakusan6111 <sakusan3rd@gmail.com>
2022-11-11 21:59:55 +0900

    2nd update

--- src/App.js
+++ src/App.js
@@ -13,11 +13,9 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
         </header>
+        <hl>Sample App by Yoshi</hl>
+        <p>This is trial by Yoshi</p>
       </div>
     );
   }
